469a2a3dfc702fbc50bb71ec08a3ed13443f42aa
Gabriel <gblsilvalopes@gmail.com>
2021-07-08 23:41:44 -0300

    ✨ Adiciona rotas para api e altera o status da controller de produtos para wip.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/teste', [ProdutoController::class, 'index']);
+
+Route::post('/teste', [ProdutoController::class, 'store']);
+
+Route::put('/teste/{id}', function ($id) {
+    return "Testando id: {$id}";
+});
+
+Route::delete('/teste/{id}', function ($id) {
+    return "id: {$id} deletado com sucesso!";
 });
